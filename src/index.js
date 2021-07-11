bef27f6de2fe15f3f2762589ed64b7386cd382b6
CuentaBorrada <amateo1982@yahoo.es>
2021-07-11 19:54:01 +0200

    Refactor del script js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,46 @@
+function setCurrentLoading({
+  currentValue,
+  loadingLeft,
+  loadingRight,
+  loadingValue,
+}) {
+  if (currentValue + 1 <= 50) {
+    loadingRight.style.transform = `rotate(${
+      (currentValue + 1) * 3.6 + 180
+    }deg)`;
+  } else {
+    loadingLeft.style.transform = `rotate(${(currentValue + 1) * 3.6}deg)`;
+  }
+
+  loadingValue.innerHTML = `${currentValue + 1}%`;
+}
+
+function simulateProcessLoading({ loadingLeft, loadingRight, loadingValue }) {
+  let totalPercentage = Array.from(Array(100).keys());
+
+  totalPercentage.reduce((previousPromise, currentValue) => {
+    return previousPromise.then(
+      () =>
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            setCurrentLoading({
+              currentValue,
+              loadingLeft,
+              loadingRight,
+              loadingValue,
+            });
+
+            resolve();
+          }, 50);
+        })
+    );
+  }, Promise.resolve());
+}
+
 window.onload = function () {
   const loadingLeft = document.querySelector(".circular-loading__left-inner");
   const loadingRight = document.querySelector(".circular-loading__right-inner");
   const loadingValue = document.querySelector(".circular-loading-value");
 
-  let percentageSimulation = Array.from(Array(100).keys());
-
-  const percentage = percentageSimulation.reduce(
-    (previousPromise, currentValue) => {
-      return previousPromise.then(
-        () =>
-          new Promise((resolve, reject) => {
-            setTimeout(() => {
-              if (currentValue + 1 <= 50) {
-                loadingRight.style.transform = `rotate(${
-                  (currentValue + 1) * 3.6 + 180
-                }deg)`;
-              } else {
-                loadingLeft.style.transform = `rotate(${
-                  (currentValue + 1) * 3.6
-                }deg)`;
-              }
-
-              loadingValue.innerHTML = `${currentValue + 1}%`;
-
-              resolve();
-            }, 50);
-          })
-      );
-    },
-    Promise.resolve()
-  );
-
-  // alert("Loading filished");
+  simulateProcessLoading({ loadingLeft, loadingRight, loadingValue });
 };
